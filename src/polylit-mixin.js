386f8d97c5a7b9d5d5c4b1b12130d6205bbe9dfa
web-padawan <iamkulykov@gmail.com>
2021-09-24 13:11:58 +0300

    Add readOnly property support and tests

--- src/polylit-mixin.js
+++ src/polylit-mixin.js
@@ -1,7 +1,37 @@
 import { dedupeMixin } from '@open-wc/dedupe-mixin';
 
+function upper(name) {
+  return name[0].toUpperCase() + name.substring(1);
+}
+
 const PolylitMixinImplementation = (superclass) => {
   class PolylitMixinClass extends superclass {
+    static getPropertyDescriptor(name, key, options) {
+      const defaultDescriptor = super.getPropertyDescriptor(name, key, options);
+
+      if (options.readOnly) {
+        const setter = defaultDescriptor.set;
+
+        this.addInitializer((instance) => {
+          // This is run during construction of the element
+          instance['_set' + upper(name)] = function (value) {
+            setter.call(instance, value);
+          };
+        });
+
+        return {
+          get: defaultDescriptor.get,
+          set() {
+            // Do nothing, property is read-only.
+          },
+          configurable: true,
+          enumerable: true
+        };
+      }
+
+      return defaultDescriptor;
+    }
+
     /** @protected */
     ready() {
       if (super.ready) {
--- test/polylit-mixin.test.js
+++ test/polylit-mixin.test.js
@@ -28,4 +28,54 @@ describe('PolylitMixin', () => {
       expect(readySpy.calledOnce).to.be.true;
     });
   });
+
+  describe('readOnly', () => {
+    let element;
+
+    const tag = defineCE(
+      class extends PolylitMixin(LitElement) {
+        static get properties() {
+          return {
+            helper: {
+              type: String
+            },
+
+            value: {
+              type: String,
+              readOnly: true
+            }
+          };
+        }
+
+        render() {
+          return html`${this.value}${this.helper}`;
+        }
+      }
+    );
+
+    beforeEach(async () => {
+      element = await fixture(`<${tag}></${tag}>`);
+    });
+
+    it('should not set property marked as readOnly using setter', async () => {
+      element.value = 'foo';
+      await element.updateComplete;
+      expect(element.value).to.not.equal('foo');
+      expect(element.shadowRoot.textContent).to.not.equal('foo');
+    });
+
+    it('should set property marked as readOnly using private method', async () => {
+      element._setValue('foo');
+      await element.updateComplete;
+      expect(element.value).to.equal('foo');
+      expect(element.shadowRoot.textContent).to.equal('foo');
+    });
+
+    it('should set property not marked as readOnly using setter', async () => {
+      element.helper = 'bar';
+      await element.updateComplete;
+      expect(element.helper).to.equal('bar');
+      expect(element.shadowRoot.textContent).to.equal('bar');
+    });
+  });
 });
